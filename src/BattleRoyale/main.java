6c1caedf94434f0bf955cbf5a303be9f6c7e0b41
MatteFlorian <matteflorian@live.fr>
2017-11-29 15:01:20 +0100

    interface grafique cool

--- src/BattleRoyale/main.java
+++ src/BattleRoyale/main.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-package BattleRoyale;
-
-import javax.swing.JFrame;
-
-/**
- * Projet JAVA Semestre1 M1
- * @author MATTE FLORIAN, MARISSAL LOIC
- */
-
-public class main {
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.InterruptedException
-     */
-    public static void main(String[] args) throws InterruptedException {
-        
-        JFrame intG ;
-        //while(true){
-        BattleRoyale platinum = new BattleRoyale(0,4,0,5,0,0,0); //nbr_soigneur, nbr_piegeur, nbr_normal, nbr_trouillard
-        platinum.lancer(1);
-    }  
-}
-=======
 package BattleRoyale;
 
 import javax.swing.JFrame;
@@ -44,4 +20,3 @@ public class main {
         platinum.lancer(1);
     }  
 }
->>>>>>> c06e1530666578c76bcbbff9e3e99bfbb2482e03
